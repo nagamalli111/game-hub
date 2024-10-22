58fb3ce5f2448ffae69a3d51bf02a84d069c6816
Nagamalleswara rao Mathi <matty@Nagamalleswaras-MacBook-Pro.local>
2024-10-22 16:55:57 +0530

    fetch platforms

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GameGrid from "./Components/GameGrid";
 import GenreList from "./Components/GenreList";
 import { useState } from "react";
 import { Genre } from "./Hooks/useGenres";
+import PlatformSelector from "./Components/PlatformSelector";
 
 function App() {
   const [genre, selectedGenre] = useState<Genre | null>(null);
@@ -31,6 +32,7 @@ function App() {
           </GridItem>
         </Show>
         <GridItem area="main">
+          <PlatformSelector />
           <GameGrid selectedGenre={genre} />
         </GridItem>
       </Grid>
